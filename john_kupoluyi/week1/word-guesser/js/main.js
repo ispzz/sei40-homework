4491a5328a30c320757b64fa549ca774d35a63f1
John Kupoluyi <o.johnkupoluyi@gmail.com>
2020-11-05 13:48:33 +1100

    refactored to use the counter approach

--- john_kupoluyi/week1/word-guesser/js/main.js
+++ john_kupoluyi/week1/word-guesser/js/main.js
@@ -5,9 +5,10 @@ console.log('The javascript has loaded!');
 // The Word Guesser
 let totalReward = 0;
 let hangman = 0;
-let wordToGuess = 'bandage';
-let wordArr = wordToGuess.toLowerCase().split('');
-let guessArr = [];
+let counter = 0;
+const wordToGuess = 'bandage';
+const wordArr = wordToGuess.toLowerCase().split('');
+const guessArr = [];
 
 for(let i = 0; i < wordArr.length; i++){
   guessArr.push('_');
@@ -18,57 +19,27 @@ const guessLetter = function(guess){
   guess = guess.toLowerCase();
 
   if(guess.length > 1){
-    console.log('You can only guess a letter at a time');
+    console.log('You can only guess a letter at a time. Try again.');
     return;
   }
 
   if(hangman === 6){
-    hangmanAscii = [`
-        +---+
-        |   |
-            |
-            |
-            |
-            |
-      =========
-      `
-    ]
-
-    console.log(`Sorry mate... you lost!`);
-    console.log(hangmanAscii[0]);
+    showHangman();
     return;
   }
 
-  if(gameOver(wordArr)){
+  if(counter === wordToGuess.length){
     console.log('GAME OVER!!!');
     return;
   };
 
   if(wordArr.includes(guess)){
     hangman -= 1;
+    counter += 1;
     let idx = wordArr.indexOf(guess);
 
     guessArr[idx] = guess;
     wordArr.splice(idx, 1, '*');
-
-    //===========Code below solved/replaced with line 12-14===============//
-    // for(let i = 0; i < guessArr.length; i++){
-    //   if(guessArr[i] === undefined){
-    //     currGuess += '_,';
-    //   } else {
-    //     currGuess += guessArr[i] + ',';
-    //   }
-    // } //for
-    //
-    // if(guessArr.length < wordArr.length){
-    //   let diff = wordArr.length - guessArr.length;
-    //   for(let i = 1; i <= diff; i++){
-    //     currGuess += '_,'
-    //   }
-    // } //if
-    //
-    // currGuess = currGuess.slice(0, currGuess.length - 1);
-    //============================================================//
 
     if(totalReward === 0){
       totalReward = currReward;
@@ -78,10 +49,13 @@ const guessLetter = function(guess){
 
     console.log('Congrats! After your guess, this is the board:');
     console.log(guessArr.join(), 'Reward: $' + totalReward);
-    wonGame(wordArr);
+
+    if(counter === wordToGuess.length){
+      console.log('Congratulations. You won the game!');
+    };
 
   } else if(guessArr.includes(guess)){
-    console.log("You've guessed that letter already. Try again.")
+    console.log("You've guessed that letter already. Please try again.")
     return;
 
   } else {
@@ -96,20 +70,17 @@ const guessLetter = function(guess){
 
 } //guessLetter()
 
-const wonGame = function(arr){
-  if(checkArr(arr)){
-    console.log('Congratulations. You won the game!');
-  }
-} //wonGame()
-
-const gameOver = function(arr){
-  if(checkArr(arr)){
-    return true;
-  }
-} //gameOver()
-
-const checkArr = function(arr){
-  return arr.every(function(el){
-    return el === '*';
-  })
-} //checkArr()
+const showHangman = function(){
+  hangmanAscii = [`
+      +---+
+      |   |
+          |
+          |
+          |
+          |
+    =========
+    `
+  ]
+  console.log(`Sorry mate... you lost!`);
+  console.log(hangmanAscii[0]);
+} //showHangman()
