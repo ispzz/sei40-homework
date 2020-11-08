431c33392fdd98034239c3cf9d8166e3a01f0d7f
CarissaPalu <73864474+CarissaPalu@users.noreply.github.com>
2020-11-08 18:36:43 +1100

    Add files via upload

---
+++ carissa_palu/main.js
@@ -0,0 +1,41 @@
+console.log("The local result is:" + localResult);
+}
+
+squareNumber(3);
+console.log(localResult); //9
+
+const squareNumber(num1) = function(num1) {
+return num1**2;
+}
+
+
+halfNumber(5);
+console.log(localResult); //2.5
+}
+const halfNumber(num1) = function(num1/2) {
+  return num1/2;
+}
+
+function percentOf(num2)
+let localResult = num1/100 * num2;
+
+percentOf(4)
+console.log("The local result is:" + localResult);
+//2}
+const percentOf(num1) = function(num1) {
+return num1/100*2;
+}
+
+function areaOfCircle(the radius) = function (the radius)
+let localResult = Pi*radius**2;
+
+console.log("The local result is:" + localResult);
+}
+const areaOfCircle(the radius) = function (the radius) {return Pi*radius**2;}
+areaOfCircle(2);
+console.log(localResult); //12.566370614359172
+}
+
+const areaOfCircle(the radius) = function(the radius) {
+  return Pi*radius**2;
+}
