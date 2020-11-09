47b92093a72914154e246a1f0362272ac709db2c
Alvaro Tovar <atovar389@gmail.com>
2020-11-09 11:30:05 +1100

    homeworks week1 bank and js bank

--- alvaro_tovar/week1/javascript-bank/js/main.js
+++ alvaro_tovar/week1/javascript-bank/js/main.js
@@ -1,4 +1,48 @@
-console.log("Javascript Bank loaded!");
+// console.log("Javascript Bank loaded!");
+//
+// // My code
+//
+// const bank = {
+//
+//   accounts: [
+//     {
+//       name: "John",
+//       balance: 20000
+//     },
+//     {
+//       name: "Manda",
+//       balance: 100000
+//     },
+//     {
+//       name: "David",
+//       balance: 50000
+//     }
+//   ], //accounts
+// }; // bank
+//
+//   //Sum all balances function
+//
+//   const totalSumAccounts = function(){
+//     let totalBankBalance = 0;
+//     for( let i = 0; i < bank.accounts.length; i++){
+//       totalBankBalance = totalBankBalance + bank.accounts[i].balance;
+//     }
+//     console.log(`Total balance in all accounts is ${totalBankBalance}`)
+//   },
+//   totalSumAccounts();
+//
+// //Add new account function
+//
+// const addAccount = function(newName, newBalance){
+//   newAccount = {name: newName, balance: newBalance};
+//   bank.accounts.push(newAccount);
+//   console.log(newAccount)
+// }
+// addAccount();
+
+// Solution
+
+console.log('GA Bank! Give us your money!!!');
 
 // JavaScript Bank
 // In this homework, you'll create a basic bank in Javascript. The bank has many accounts and the following capabilities that you need to write.
@@ -14,67 +58,86 @@ console.log("Javascript Bank loaded!");
 // There is no need to write a user interface. Make sure functions return values -- you may also have your functions console.log() values to help you see your code working.
 //
 // You should write a basic story through a series of JavaScript commands that shows that the methods do indeed work as expected: add some accounts, show the total balance, make some deposits and withdrawals, show the new total balance.
-//
-// Tips
-// Don't overthink this. Shorter code is probably the answer.
-//
-// Bonus
-// Ensure that the accounts cannot have negative values.
-// Write a 'transfer' on the bank that allows you to transfer amounts between two accounts.
-// Additional
-// Begin exploring the JavaScript Koans. Fork, clone and start trying them.
 
 const bank = {
 
   accounts: [
-    {
-      name: "John",
-      balance: 20000
-    },
-    {
-      name: "Manda",
-      balance: 100000
-    },
-    {
-      name: "David",
-      balance: 50000
-    }
-  ], //accounts
-}; // bank
+    { name: 'Ruby', balance: 1000000 },
+    { name: 'Nookie', balance: 10 },
+    { name: 'Socks', balance: 0 },
+  ],
 
-  //Sum all balances function
+  // accounts: {
+  //   'Ruby': { balance: 0, address: '', cc: '' }
+  // },
 
-  const totalSumAccounts = function(){
-    let totalBankBalance = 0;
-    for( let i = 0; i < bank.accounts.length; i++){
-      totalBankBalance = totalBankBalance + bank.accounts[i].balance;
-    }
-    console.log(`Total balance in all accounts is ${totalBankBalance}`)
-  },
-  totalSumAccounts();
+  getBankGrandTotal: function(){
 
-//Add new account function
+    let grandTotal = 0;
+    for( let i = 0; i < this.accounts.length ; i++ ){
+      const currentAccount = this.accounts[i];
+      grandTotal += currentAccount.balance;
+    } // for
 
-const addAccount = function(newName, newBalance){
-  newAccount = {name: newName, balance: newBalance};
-  bank.accounts.push(newAccount);
-  console.log(newAccount)
-}
-addAccount();
+    console.log('grandTotal:', grandTotal);
 
-//bank.account.push( { name: 'Terry', balance: 10000 } );
+    return grandTotal;
+
+  }, // getBankGrandTotal()
+
+  addAccount: function( accountName ){
+
+    // const newAccount = { name: accountName, balance: 0 };
+    // this.accounts[ this.accounts.length ] = newAccount;
+    this.accounts.push({
+      name: accountName,
+      balance: 0
+    });
+
+  }, // addAccount()
+
+  deposit: function( accountName, amount ){
+
+    // 1. Loop through all the accounts until you find
+    // one whose .name matches the accountName given, and store it
+    // 2. Update the balance on that account accordingly (+amount)
+
+    // We need this to keep track of the correct account
+    // when we we find it in the loop, so we can use it after
+    // the loop is finished.
+    let account = null;
+
+    for( let i = 0; i < this.accounts.length; i++ ){
+      if( this.accounts[i].name === accountName ){
+        account = this.accounts[i];
+        break;
+      } // if
+    } // for
+
+    if( account === null ){
+      // We did not find a matching account
+      // (i.e. the account variable never got changed from
+      // its initial null value by the loop code)
+      console.log(`ERROR: could not find account with name ${accountName}`);
+      return false;  // this indicates the deposit failed
+    } else {
+      // Found matchinig account
+      account.balance += amount;
+      console.log(`New balance for '${account.name}': ${account.balance}`);
+      return true;  // this indicates the deposit was successful
+    } // else
+
+  }, // deposit()
+
+  withdraw: function( accountName, amount ){
+    // YOUDO
+  };
 
 
-  //bank.accounts.push("something") ;
+}; // bank object
 
 
-  // //
-  // // addAcount: function( name ){
-  // //
-  // // }, // addAcount()
-  // //
-  // // deposit: function( name, amount ){
-  //
-  // }, // deposit()
-  //
-  // etc...
+const total = bank.getBankGrandTotal(); // test it!
+
+bank.deposit( 'Ruby', 500 ); // should work
+bank.deposit( 'Luke', 500 ); // should give an error (no such account)
