170c38ae6e2ff15fe86911e3f4067f6055c9f2a5
CarissaPalu <73864474+CarissaPalu@users.noreply.github.com>
2020-11-12 17:31:48 +1100

    HOME WORK - CASH REGISTER

---
+++ carissa_palu/main.js
@@ -0,0 +1,39 @@
+# The Cash Register
+
+Write a function called cashRegister that takes a shopping cart object.
+The object contains item names and prices (itemName: itemPrice).
+The function should return the total price of the shopping cart.
+
+const cartForParty = {
+  mousse cake: "10.00",
+  watermelon:"5.00"
+  honeydew:"3.00"
+  rockmelon:"4.00"
+  strawberries: "4.00"
+  pawpaw: "8.00",
+  lemonade: "1.00",
+  mint: "3.00"
+  cranberry juice: "2.85",
+  passion fruit cheese cake slices: ""
+  }
+}
+Output
+
+
+
+Example
+
+```
+// Input
+const cartForParty = {
+  banana: "1.25",
+  handkerchief: ".99",
+  Tshirt: "25.01",
+  apple: "0.60",
+  nalgene: "10.34",
+  proteinShake: "22.36"
+};
+
+// Output
+cashRegister(cartForParty); // 60.55
+```
