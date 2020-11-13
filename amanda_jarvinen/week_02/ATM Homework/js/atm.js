2f6374f4607e5b74a2d554d3b87dbe3b3b3e2677
Amanda Jarvinen <nimisaya@outlook.com>
2020-11-14 00:54:58 +1100

    Remove extra comments and add TODOs

--- amanda_jarvinen/week_02/ATM Homework/js/atm.js
+++ amanda_jarvinen/week_02/ATM Homework/js/atm.js
@@ -4,6 +4,9 @@ const name = 'Zara';
 let checkingAmount = 0;
 let savingsAmount = 0;
 
+// TODO: Refactor and place relevant code within doc...ready() func
+
+
 // Update display colour to red if balance is $0 otherwise grey
 const updateBackground = function(accountType){
   if(bank.checkAccountBalance(name, accountType) === 0){
@@ -22,8 +25,6 @@ const updateDisplay = function(accountType){
     accountID = '#balance1';
   }
 
-  // const accountID = `#${accountType}Account.balance`;
-
   $(`${accountID}`).text(`$${balance}`);
 
   console.log('Display:', accountType);
@@ -36,24 +37,16 @@ const createAccount = function(){
   bank.addAccount(name, savingsAmount, 'savings');
   bank.addAccount(name, checkingAmount, 'checking');
 
-
   // Display initial balance
   updateDisplay('checking');
   updateDisplay('savings');
 }; // createAccount()
-
-
 
 // Get value from input
 const getInput = function(){
   console.log(`amount user inputed:`, this.value)
   return this.value;
 }; // getInput()
-
-
-
-
-
 
 // Display logic
 createAccount();
@@ -98,46 +91,3 @@ $('#savingsWithdraw').on('click', function(){
   updateDisplay('checking');
   updateDisplay('savings');
 });
-
-
-
-
-
-//
-//
-//
-//
-//
-// $('#checkingDeposit').on('click', deposit(event, 'checking', 10));
-// // $('#savingsDeposit').on('click', deposit('savings', $('#savingsAmount').text()));
-// //
-// // // Withdraw money
-// // $('#checkingWithdraw').on('click', withdraw('checking', $('#checkingAmount').text()));
-// // $('#savingsWithdraw').on('click', withdraw('savings', $('#savingsAmount').text()));
-// // Deposit money
-// const deposit = function(this, accountType, amount){
-//   // if (accountType === 'checking'){
-//   //   const amount = checkingInput;
-//   // } else {
-//   //   const amount = savingsInput;
-//   // }
-//   console.log(`Deposit pressed on ${accountType}`)
-//
-//   const updatedBalance = bank.deposit(name, parseInt(amount), `${accountType}`);
-//
-//   updateDisplay(`${accountType}`);
-// } // deposit()
-//
-// // Withdraw money
-// const withdraw = function(event, accountType, amount){
-//   // if (accountType === 'checking'){
-//   //   const amount = checkingInput;
-//   // } else {
-//   //   const amount = savingsInput;
-//   // }
-//   console.log(`Withdraw pressed on ${accountType}`)
-//
-//   const updatedBalance = bank.withdraw(name, parseInt(amount), `${accountType}`);
-//
-//   updateDisplay(accountType);
-// } // deposit()
