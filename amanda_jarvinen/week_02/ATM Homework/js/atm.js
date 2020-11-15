1c9d00461d4b02c25732626bc28a42a9762f39ac
Amanda Jarvinen <nimisaya@outlook.com>
2020-11-16 09:51:03 +1100

    Fix display colour and remove print statements

--- amanda_jarvinen/week_02/ATM Homework/js/atm.js
+++ amanda_jarvinen/week_02/ATM Homework/js/atm.js
@@ -1,93 +1,93 @@
-console.log('ATM');
 
-const name = 'Zara';
+const name = 'Emily';
 let checkingAmount = 0;
 let savingsAmount = 0;
 
-// TODO: Refactor and place relevant code within doc...ready() func
+$(document).ready(function(){
+
+  // Update display colour to red if balance is $0 otherwise grey
+  const updateBackground = function(accountType){
+    if (accountType === 'savings'){
+      accountID = '#balance2';
+    } else {
+      accountID = '#balance1';
+    }
+
+    if(bank.checkAccountBalance(name, accountType) <= 0){
+      $(accountID).addClass('zero-background');
+      $(accountID).removeClass('money-background');
+    } else {
+      $(accountID).addClass('money-background');
+      $(accountID).removeClass('zero-background');
+    }
+  }; // updateBackground()
+
+  const updateDisplay = function(accountType){
+    const balance = bank.checkAccountBalance(name, accountType);
+
+    if (accountType === 'savings'){
+      accountID = '#balance2';
+    } else {
+      accountID = '#balance1';
+    }
+
+    $(`${accountID}`).text(`$${balance}`);
+    updateBackground(accountType);
+
+  }; // updateDisplay()
+
+  const createAccount = function(){
+    bank.addAccount(name, savingsAmount, 'savings');
+    bank.addAccount(name, checkingAmount, 'checking');
+
+    // Display initial balance
+    updateDisplay('checking');
+    updateDisplay('savings');
+
+  }; // createAccount()
+
+  // Get value from input
+  const getInput = function(){
+    return this.value;
+  }; // getInput()
 
 
-// Update display colour to red if balance is $0 otherwise grey
-const updateBackground = function(accountType){
-  if(bank.checkAccountBalance(name, accountType) === 0){
-    $(`#${accountType}Account.balance`).addClass('zero-background');
-  } else {
-    $(`#${accountType}Account.balance`).addClass('money-background');
-  }
-}; // updateBackground()
-
-const updateDisplay = function(accountType){
-  const balance = bank.checkAccountBalance(name, accountType);
-
-  if (accountType === 'savings'){
-    accountID = '#balance2';
-  } else {
-    accountID = '#balance1';
-  }
-
-  $(`${accountID}`).text(`$${balance}`);
-
-  console.log('Display:', accountType);
-  console.log('Balance:', balance);
-
-  updateBackground(accountType);
-}; // updateDisplay()
-
-const createAccount = function(){
-  bank.addAccount(name, savingsAmount, 'savings');
-  bank.addAccount(name, checkingAmount, 'checking');
-
-  // Display initial balance
-  updateDisplay('checking');
-  updateDisplay('savings');
-}; // createAccount()
-
-// Get value from input
-const getInput = function(){
-  console.log(`amount user inputed:`, this.value)
-  return this.value;
-}; // getInput()
-
-// Display logic
-createAccount();
-const balance = bank.checkAccountBalance(name, 'checking');
-// $(`#${accountType}Account.balance`).text(`$${balance}`);
-$('balance1').text(`$`+ balance);
 
 
-// Get input value
-$(`#checkingAmount`).on('keyup', function(){
-  console.log('Checking amount', $(this).val());
-   checkingAmount = $(this).val();
-});
+  // DISPLAY LOGIC
+  createAccount();
 
-$(`#savingsAmount`).on('keyup', function(){
-    console.log('Savings amount', $(this).val());
-    savingsAmount = $(this).val();
-});
+  // Get input value
+  $(`#checkingAmount`).on('keyup', function(){
+     checkingAmount = $(this).val();
+  });
 
-// Deposit money
-$('#checkingDeposit').on('click', function(){
-  console.log('checking amount:', checkingAmount);
-  const updatedBalance = bank.deposit(name, parseInt(checkingAmount), 'checking');
-  console.log('UpdatedBalance after deposit:', updatedBalance);
-  updateDisplay('checking');
-  updateDisplay('savings');
-});
-$('#savingsDeposit').on('click', function(){
-  const updatedBalance = bank.deposit(name, parseInt(savingsAmount), 'savings');
-  updateDisplay('checking');
-  updateDisplay('savings');
-});
+  $(`#savingsAmount`).on('keyup', function(){
+      savingsAmount = $(this).val();
+  });
 
-// Withdraw money
-$('#checkingWithdraw').on('click', function(){
-  const updatedBalance = bank.withdraw(name, parseInt(checkingAmount), 'checking');
-  updateDisplay('checking');
-  updateDisplay('savings');
-});
-$('#savingsWithdraw').on('click', function(){
-  const updatedBalance = bank.withdraw(name, parseInt(savingsAmount), 'savings');
-  updateDisplay('checking');
-  updateDisplay('savings');
+  // Deposit money
+  $('#checkingDeposit').on('click', function(){
+    const updatedBalance = bank.deposit(name, parseInt(checkingAmount), 'checking');
+    updateDisplay('checking');
+    updateDisplay('savings');
+  });
+  $('#savingsDeposit').on('click', function(){
+    const updatedBalance = bank.deposit(name, parseInt(savingsAmount), 'savings');
+    updateDisplay('checking');
+    updateDisplay('savings');
+  });
+
+  // Withdraw money
+  $('#checkingWithdraw').on('click', function(){
+    const updatedBalance = bank.withdraw(name, parseInt(checkingAmount), 'checking');
+    updateDisplay('checking');
+    updateDisplay('savings');
+  });
+  $('#savingsWithdraw').on('click', function(){
+    const updatedBalance = bank.withdraw(name, parseInt(savingsAmount), 'savings');
+    updateDisplay('checking');
+    updateDisplay('savings');
+  });
+
 });
