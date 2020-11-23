364714c66fe29ca4b19927d8ad43b8b556242562
Amanda Jarvinen <nimisaya@outlook.com>
2020-11-23 17:42:46 +1100

    Display menu

---
+++ amanda_jarvinen/week_04/calculator.rb
@@ -0,0 +1,13 @@
+# Display menu and retrieve inputs
+def display_menu
+  puts "***********************"
+  puts "  CALC the CALCULATOR   ".magenta
+  puts "***********************"
+  puts "Menu:"
+  puts "+ : Addition"
+  puts "- : Subtraction"
+  puts "* : Multiplication"
+  puts "/ : Division"
+  print "Which operation would you like to perform?  "
+  gets.chomp
+end
