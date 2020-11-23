1745f0855015f28cee76b65c7e5f28a11b644092
Amanda Jarvinen <nimisaya@outlook.com>
2020-11-23 17:44:31 +1100

    Add addition operation

--- amanda_jarvinen/week_04/calculator.rb
+++ amanda_jarvinen/week_04/calculator.rb
@@ -15,9 +15,16 @@ def getOperator
   print "Enter a number: "
   gets.to_f
 end
+# Calculations
+def add(first_number, second_number)
+  first_number + second_number
+end
 def calculator ()
   operation = display_menu
   first_number = getOperator
   second_number = getOperator
+  result = case operation
+  when '+' then add first_number, second_number
+  result
 end
 puts calculator
