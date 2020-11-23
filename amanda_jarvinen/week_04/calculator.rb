fa8573931a148792f81c30573015cbd2293d1bba
Amanda Jarvinen <nimisaya@outlook.com>
2020-11-23 17:46:19 +1100

    Add task description and requirements

--- amanda_jarvinen/week_04/calculator.rb
+++ amanda_jarvinen/week_04/calculator.rb
@@ -1,3 +1,21 @@
+# Calculator
+# Explanation
+# You will be building a calculator which can perform multiple arithmetic operations. Your program should allow the user to choose which operation to perform, enter the values on which the operation is performed, and output the result.
+# Specification:
+# A user should be given a menu of operations
+# A user should be able to choose from the menu
+# A user should be able to enter numbers to perform the operation on
+# A user should be shown the result
+# The menu should be displayed again, and process should continue until the user selects a quit option from the menu
+# Phase 1
+# Calculator functionality
+# Calculator should be able to do basic arithmetic (+,-, *, /)
+# Phase 2
+# Advanced Calculator functionality
+# Calculator should be able to do basic arithmetic (exponents, square roots)
+require 'colorize'
+
+
 # Display menu and retrieve inputs
 def display_menu
   puts "***********************"
