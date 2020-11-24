a3091161bac561978a23fd54043846b16ecc4621
Amanda Jarvinen <nimisaya@outlook.com>
2020-11-24 23:42:22 +1100

    Refactor

--- amanda_jarvinen/week_04/mta.rb
+++ amanda_jarvinen/week_04/mta.rb
@@ -9,59 +9,59 @@ mta = {
 
 def calculate_num_stops stations
   stations.length unless stations.nil?
-end
+end # calculate_num_stops
 
-def get_stations network, line, origin, destination
-  origin_idx = network[line].index(origin)
-  destination_idx = network[line].index(destination)
+def get_stations network, line, start, finish
+  stations = network[line]
+  start_idx = stations.index(start)
+  finish_idx = stations.index(finish)
 
-  case
-  when origin_idx < destination_idx then network[line][(origin_idx + 1)..destination_idx]
-  when origin_idx > destination_idx then network[line][(destination_idx)..origin_idx - 1].reverse!
-  else puts "You are at your destination"
+  if start_idx < finish_idx
+    stations[(start_idx + 1)..finish_idx]
+  else
+    stations[(finish_idx)..start_idx - 1].reverse!
   end
-end
+end # get_stations
 
-def single_line line, origin, destination, network
-  stations = get_stations network, line, origin, destination
+def single_line line, start, finish, network
+  stations = get_stations network, line, start, finish
   total_stops = calculate_num_stops stations
 
   {stations: stations, total_stops: total_stops}
-end
+end # single_line
 
-def plan_trip o_line, origin, d_line, destination, network
+def plan_trip start_line, start, end_line, finish, network
 
-  if o_line == d_line
-    trip = single_line o_line, origin, destination, network
+  puts "You must travel through the following stops on the #{start_line} line:".blue
 
-    puts "You must travel through the following stops on the #{o_line.upcase} line:"
+  if start_line == end_line
+    trip = single_line start_line, start, finish, network
+    total_stops = trip[:total_stops]
     puts trip[:stations]
-    puts "Total stations: #{trip[:total_stops]}."
 
   else
-    exchange = (network[o_line] & network[d_line]).join # Intersection (common elements of 2 arrays)
-    first_leg = single_line o_line, origin, exchange, network
+    exchange = (network[start_line] & network[end_line]).join # Intersection (common elements of 2 arrays)
+    first_leg = single_line start_line, start, exchange, network
     first_leg[:stations].pop # Remove exchange station
-    last_leg = single_line d_line, exchange, destination, network
+    last_leg = single_line end_line, exchange, finish, network
     stations = (first_leg[:stations] + last_leg[:stations]).join(', ')
     total_stops = first_leg[:total_stops] + last_leg[:total_stops]
 
-    puts "You must travel through the following stops on the #{o_line.upcase} line:"
     puts first_leg[:stations]
-    puts "Please exchange to #{d_line.upcase} at #{exchange}. Then travel:"
+    puts "Please exchange to #{end_line.upcase} at #{exchange}. Then travel:".blue
     puts last_leg[:stations]
-    puts "Total stations: #{total_stops}."
   end
 
-  puts "Have a great day!\n"
-end
+  puts "Total stations: #{total_stops}".blue
+  puts "Have a great day!\n\n".magenta
+end # plan_trip
 
 def display_title
   puts "\n*************************************"
   puts "     WELCOME TO MTA Trip Planner ".magenta
   puts " 🚂 We choo...choose to move you 😃 "
   puts "*************************************\n\n"
-end
+end # display_title
 
 def display_menu
   puts "Menu: ".magenta
@@ -70,29 +70,42 @@ def display_menu
   puts "q : Quit"
   print "\nWhich would you like to do?  "
   gets.chomp
-end
+end # display_menu
 
 def plan_trip_prompt network
-  print "What line (:n, :l or 6) are you starting on? "
-  start_line = gets.to_sym
+  print "What line (n, l or 6) are you starting on? "
+  start_line = gets.chomp
 
-  print "Which station are you on now? "
-  start_station = gets.chomp
-
-  print "What is your transfer line (:n, :l or 6)? "
-  end_line = gets.to_sym
-
-  print "Where do you want to get off? "
-  end_station = gets.chomp
-
-  # Edge case: User is already at destination
-  if start_station == end_station
-    puts "\nYou are already at your destination".red
-    main_menu
+  if start_line == "6"
+    start_line = start_line.to_i
+  else
+    start_line = start_line.to_sym
   end
 
-  plan_trip start_line, start_station, end_line, end_station, network
-end
+  print "Which station are you on now? "
+  start = gets.chomp
+
+  print "What is your transfer line (n, l or 6)? "
+  end_line = gets.chomp
+
+  if end_line == "6"
+    end_line = end_line.to_i
+  else
+    end_line = end_line.to_sym
+  end
+
+  print "Where do you want to get off? "
+  finish = gets.chomp
+  puts "\n"
+
+  # Edge case: User is already at destination
+  if start == finish
+    puts "\nYou are already at your destination".red
+    main_menu
+  else
+    plan_trip start_line, start, end_line, finish, network
+  end
+end # plan_trip_prompt
 
 def run_tests(network)
   puts "\nRUNNING TESTS".magenta
@@ -102,13 +115,6 @@ def run_tests(network)
   plan_trip(:n, 'Union Square', :n, 'Union Square', network)
 
 
-
-
-  
-  # Single-line and forward direction
-  # trip_sf = single_line(:n, '34th', 'Union Square', network)
-  # p trip_sf
-
   # Single-line and reverse direction
   puts "\ntest: (:n, 'Union Square', :n, '34th', 'mta')".blue
   plan_trip(:n, 'Union Square', :n, '34th', network)
@@ -117,8 +123,11 @@ def run_tests(network)
   puts "\ntest: (n, '34th', l, '1st', 'mta')".blue
   plan_trip(:n, '34th', :l, '1st', network)
 
+  puts "\ntest: (6, 'Grand Central', l, '1st', 'mta')".blue
+  plan_trip(6, 'Grand Central', :l, '1st', network)
+
   puts "\nEND OF TESTS\n".magenta
-end
+end # run_tests
 
 def main_menu network
   loop do
