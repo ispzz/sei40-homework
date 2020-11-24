8f970bc3caf0560484c075fe4b48830d74d9ef84
Sam Skinner <sam.sjskinner@gmail.com>
2020-11-25 09:25:37 +1100

    Updated Tuesday Homework

--- sam_skinner/week4/mta-lab/mta-lab.rb
+++ sam_skinner/week4/mta-lab/mta-lab.rb
@@ -5,8 +5,9 @@ $mta =  {"N" => ['Times Square', '34th', '28th', '23rd', 'Union Square', '8th'],
          "6" => ['Grand Central', '33rd', '28th', '23rd', 'Union Square', 'Astor Place']}
 
 #====Display Logic====
-def plan_trip(start_line, start_station, end_line, end_station)
-  trip_destinations = line_check(start_line, start_station, end_line, end_station)
+def plan_trip start_line, start_station, end_line, end_station
+  trip_destinations = line_check start_line, start_station, end_line, end_station
+  puts "=====MTA Trip Planner Started!====="
   puts "Your journey takes you through the following stations on the #{start_line} Line: #{trip_destinations[0].join(', ')}.".green
   if start_line != end_line
     puts "Change at Union Square".yellow
@@ -16,31 +17,31 @@ end #plan_trip
 
 
 #====Business Logic====
-def check_reverse(line, start_station, end_station)
+def check_reverse line, start_station, end_station
   if $mta[line].index(start_station) < $mta[line].index(end_station)
     #stations run forward through array
-    line_journey(line, start_station, end_station)
+    line_journey line, start_station, end_station
   else
     #stations run in reverse through array
-    line_journey(line, end_station, start_station).reverse
+    line_journey line, end_station, start_station.reverse
   end
 end #check_reverse
 
 
-def line_check(start_line, start_station, end_line, end_station)
+def line_check start_line, start_station, end_line, end_station
   if start_line == end_line
     #one line trip
-    first_trip = check_reverse(start_line, start_station, end_station)
+    first_trip = [check_reverse(start_line, start_station, end_station)]
   else
     #two line trip
-    first_trip = check_reverse(start_line, start_station, "Union Square")
-    second_trip = check_reverse(end_line, "Union Square", end_station)
+    first_trip = check_reverse start_line, start_station, "Union Square"
+    second_trip = check_reverse end_line, "Union Square", end_station
     total_trip = [first_trip, second_trip]
   end
 end #line_check
 
 
-def line_journey(line, start_station, end_station)
+def line_journey line, start_station, end_station
   start_pos = $mta[line].index start_station
   end_pos = $mta[line].index end_station
   journey = $mta[line][start_pos..end_pos]
@@ -48,4 +49,5 @@ end #line_journey
 
 
 
+plan_trip("6", "23rd", "6", "33rd")
 plan_trip("N", "34th", "L", "8th")
