ae940d83566f03357617f15009630637ecbfe8fa
Amanda Jarvinen <nimisaya@outlook.com>
2020-11-25 19:15:10 +1100

    Add basic search form

--- amanda_jarvinen/week_04/movie_search/server.rb
+++ amanda_jarvinen/week_04/movie_search/server.rb
@@ -1,6 +1,17 @@
+
+# A route which uses the submitted search term to search themoviedb.org and output the results into the template, one result per line (probably use <ul> and <li>) - at least the title, maybe the year and overview text too
+# Optionally: also show a movie poster thumbnail for each result (you will have to do your own research to work out how to construct the full URL for a poster image; you're not the first person to attempt this)
+# Optionally: if you're feeling very brave, make each search result a link, which uses the movie ID returned in the results to go to another route on your sinatra site (like /movie/234235) - the route block can use the ID in the route URL (via params) to make another request to themoviedb.org, this time to a different endpoint to get the extended movie details (including budget etc - RTFM to find out the API URL for looking up a movie when you already have its ID from a previous search), and show these details in a template (I did say if you were feeling brave)
+
+
 require 'sinatra'
 require 'sinatra/reloader'
+require 'httparty'
 
+movies = HTTParty.get 'https://api.themoviedb.org/3/search/movie?api_key=24d863d54c86392e6e1df55b9a328755&query=alien'
+
+
+# A route which displays a blank form where the user can enter a search term; which submits to...
 get '/' do
-  "Hello world"
+  erb :movie_form
 end
