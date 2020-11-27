a777dc91c148a47b7553913d01346ab7c5bcb2f2
nimisaya <nimisaya@outlook.com>
2020-11-27 22:43:10 +1100

    Create renter table and add seed data

---
+++ amanda_jarvinen/week_04/rentals/renters.sql
@@ -0,0 +1,32 @@
+DROP TABLE IF EXISTS renters;
+
+CREATE TABLE renters (
+  "id" INTEGER PRIMARY KEY AUTOINCREMENT,
+  "first_name" TEXT,
+  "last_name" TEXT,
+  "phone_number" TEXT,
+  "paid" BOOLEAN,
+  "rent" INTEGER,
+  "created_at" TEXT DEFAULT CURRENT_TIMESTAMP,
+  "property_id" INTEGER
+);
+
+INSERT INTO renters ("first_name", "last_name", "phone_number", "paid", "rent", "property_id")
+VALUES (
+  "Trevor",
+  "Wilson",
+  "0423123994",
+  "true",
+  "630",
+  "1"
+);
+
+INSERT INTO renters ("first_name", "last_name", "phone_number", "paid", "rent", "property_id")
+VALUES (
+  "Krijstelle",
+  "Lu",
+  "0454367864",
+  "false",
+  "540",
+  "2"
+);
