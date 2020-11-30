dcd0febec964b76e183d73945201c340e928739a
Amanda Jarvinen <nimisaya@outlook.com>
2020-11-30 21:48:25 +1100

    Add magic 8 ball form

---
+++ amanda_jarvinen/week_05/games-on-rails/app/controllers/magic_controller.rb
@@ -0,0 +1,7 @@
+class MagicController < ApplicationController
+    # Magic 8 ball takes user's questions from a form submission and returns a positive or negative answer.
+    def ask_question
+    end
+
+    
+end # MagicController
--- amanda_jarvinen/week_05/games-on-rails/config/routes.rb
+++ amanda_jarvinen/week_05/games-on-rails/config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+#   Magic 8 Ball
+
+# Magic 8 ball takes user's questions from a form submission and returns a positive or negative answer.
+# You should make a route like /games/magic/ask which routes to magic#ask_question (i.e. you will need a controller called magic_controller.rb which has a method ask_question). This controller action will have a template which shows a form with a single text input field (the template will have to be called app/views/magic/ask_question.html.erb).
+# The form should use its action attribute to submit to another route, for example /games/magic/answer which you will have to route to an appropriate action, pulling the question out of params.
+
+# 1. Route for magic 8 ball form
+get '/games/magic/ask' => 'magic#ask_question'
+
+# 2. Route for magic 8 ball answer
+
+
+
+
 end
