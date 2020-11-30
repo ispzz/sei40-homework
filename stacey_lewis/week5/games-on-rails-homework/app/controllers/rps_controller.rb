1dacf32af0707115da27e84baef9dc12e72f92d8
Stacey Lewis <stacey.lewis@rea-group.com>
2020-12-01 10:18:52 +1100

    games on rails homework

---
+++ stacey_lewis/week5/games-on-rails-homework/app/controllers/rps_controller.rb
@@ -0,0 +1,27 @@
+class RpsController < ApplicationController
+
+def play
+end
+
+def result
+  array = ["Rock","Scissors","Paper"]
+
+  @computer_guess = array.sample
+  # puts computer_guess
+
+
+    if @computer_guess == params[:guess].to_s
+      @result = "It's a draw. If you aren't winning, your losing though right?"
+    elsif (@computer_guess == "Rock" && params[:guess] == "Paper") |
+       (@computer_guess == "Paper" && params[:guess] == "Scissors") |
+       (@computer_guess == "Scissors" && params[:guess] == "Rock")
+       @result = "You win, you are really good at this. Like, Wow."
+    else @result = "Sorry, you lose! Don't give up your day job?"
+  end
+
+
+end
+
+
+
+end
