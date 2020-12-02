77166aea6014b400b701abf6f3f9032024759767
Amanda Jarvinen <nimisaya@outlook.com>
2020-12-02 23:47:46 +1100

    Finish adding templates and add dropdown for new creations form

--- amanda_jarvinen/week_05/creativeco-app/app/controllers/coders_controller.rb
+++ amanda_jarvinen/week_05/creativeco-app/app/controllers/coders_controller.rb
@@ -4,6 +4,8 @@ class CodersController < ApplicationController
   end
 
   def create
+    Coder.create coder_params
+    redirect_to coders_path
   end
 
   def index
@@ -15,11 +17,21 @@ class CodersController < ApplicationController
   end
 
   def edit
+    @coder = Coder.find params[:id]
   end
 
   def update
+    coder = Coder.find params[:id]
+    coder.update coder_params
+    redirect_to coder_path(coder.id)
   end
 
   def destroy
+    Coder.destroy params[:id]
+    redirect_to coders_path # back to index
+  end
+
+  def coder_params
+    params.require(:coder).permit(:name, :nationality, :image_url, :bio, :portfolio_url, :instagram_url)
   end
 end
--- amanda_jarvinen/week_05/creativeco-app/app/controllers/creations_controller.rb
+++ amanda_jarvinen/week_05/creativeco-app/app/controllers/creations_controller.rb
@@ -4,7 +4,8 @@ class CreationsController < ApplicationController
   end
 
   def create
-    
+    Creation.create creation_params
+    redirect_to creations_path
   end
 
   def index
@@ -16,11 +17,22 @@ class CreationsController < ApplicationController
   end
 
   def edit
+    @creation = Creation.find params[:id]
   end
 
   def update
+    creation = Creation.find params[:id]
+    creation.update creation_params
+    redirect_to creation_path(creation.id)
   end
 
   def destroy
+    Creation.destroy params[:id]
+    redirect_to creations_path # back to index
   end
+
+  def creation_params
+    params.require(:creation).permit(:title, :medium, :image_url, :coder_id)
+  end
+
 end
