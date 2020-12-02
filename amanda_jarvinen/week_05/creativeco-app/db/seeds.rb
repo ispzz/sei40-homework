51acae7a8dacaf6f8b099cf851da64c35488b1bf
Amanda Jarvinen <nimisaya@outlook.com>
2020-12-02 21:05:28 +1100

    Add coder and creation to db

--- amanda_jarvinen/week_05/creativeco-app/db/seeds.rb
+++ amanda_jarvinen/week_05/creativeco-app/db/seeds.rb
@@ -1,7 +1,33 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# CODERS
+Coder.destroy_all # start with clean slate
+
+puts "Create Coders..."
+
+mattdesl = Coder.create!(
+    name: 'Matt Deslauriers',
+    nationality: 'Canadian',
+    portfolio_url: 'https://www.matdesl.com',
+    instagram_url: 'https://www.instagram.com/mattdesl_art/',
+    image_url: 'mattdesl.jpg',
+    bio: 'Generative artist and creative coder who created canvas-sketch, a framework for developing Generative Art with JavaScript'
+)
+
+puts "Done! Created #{Coder.count} works:"
+puts Coder.pluck(:name).join(', ')
+
+
+
+# CREATIONS
+Creation.destroy_all # start with clean slate
+
+puts "Create Creations..."
+
+Creation.create!(
+    title: 'Color-Wander',
+    medium: 'Open-source generative artowrk in Node.js and the browser',
+    image_url: 'color-wander.jpg',
+    coder_id: mattdesl.id
+)
+
+puts "Done! Created #{Creation.count} works:"
+puts Creation.pluck(:title).join(', ')
