afdbb6050d231e5e0c39d8ab0546f491d6f4b03d
nimisaya <nimisaya@outlook.com>
2020-12-14 21:37:11 +1100

    Print search term to console

--- amanda_jarvinen/week_07/movie_search/js/main.js
+++ amanda_jarvinen/week_07/movie_search/js/main.js
@@ -1,1 +1,9 @@
 console.log('Hello ', $)
+
+$(document).ready(function(){
+
+    $('#submitSearch').on('click', function() {
+        const title = $(`#searchTitle`).val();
+        console.log(`${title}`);
+    }); // submitSearch click event
+}); // document ready
