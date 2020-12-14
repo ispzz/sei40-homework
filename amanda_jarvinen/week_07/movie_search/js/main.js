ab2902f2f9f12cdf411297c0f5e7f6bfb6d71702
nimisaya <nimisaya@outlook.com>
2020-12-14 21:54:06 +1100

    Receive movie data based on search query

--- amanda_jarvinen/week_07/movie_search/js/main.js
+++ amanda_jarvinen/week_07/movie_search/js/main.js
@@ -3,7 +3,25 @@ console.log('Hello ', $)
 $(document).ready(function(){
 
     $('#submitSearch').on('click', function() {
+        
+        // Assign user search term
         const title = $(`#searchTitle`).val();
         console.log(`${title}`);
+
+        // Create an instance of the XMLHttpRequest object
+        const xhr = new XMLHttpRequest();
+
+        // Open HTTP request
+        xhr.open("GET", `https://api.themoviedb.org/3/search/movie?api_key=24d863d54c86392e6e1df55b9a328755&query=${title}`);
+        xhr.send();
+
+        xhr.onload = function() {
+            // Receive response string and parse into JS object
+            const data = JSON.parse(xhr.response);
+
+            console.log('response:', data)
+        }; // onload
+
+
     }); // submitSearch click event
 }); // document ready
