fb4a669370bc3129724f5f0c6fe853a722344b1c
nimisaya <nimisaya@outlook.com>
2020-12-15 19:51:24 +1100

    Print search term to console

--- amanda_jarvinen/week_07/giphy_api/js/main.js
+++ amanda_jarvinen/week_07/giphy_api/js/main.js
@@ -1,1 +1,9 @@
 console.log('Hello', $);
+
+$(document).ready(function(){
+
+    $('#searchBtn').on('click', function(){
+        const searchTerm = $('#searchTerm').val();
+        console.log(searchTerm)
+    });
+});
