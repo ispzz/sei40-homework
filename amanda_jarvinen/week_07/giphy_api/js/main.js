8f52e0115187ff3b3323bcb79f4e069453771d1c
nimisaya <nimisaya@outlook.com>
2020-12-15 20:54:30 +1100

    Display gifs

--- amanda_jarvinen/week_07/giphy_api/js/main.js
+++ amanda_jarvinen/week_07/giphy_api/js/main.js
@@ -8,13 +8,18 @@ $(document).ready(function(){
 
         const apiKey = '0KxC2vDq0SdT9gaua6lotHAFwYw7M5KR';
         const baseURL = 'http://api.giphy.com/v1/gifs/search?q=';
+        const limiter = '&limit=5'
+        const url = baseURL + searchTerm + '&api_key=' + apiKey + limiter
 
-        const url = baseURL + searchTerm + '&api_key=' + apiKey
-
-        console.log(url)
-
-        // $.getJSON()
-        // .done()
-        // .fail(console.warn);
+        $.getJSON(url)
+        .done(function(response){
+            // Display gif's matching search term
+            const giphs = response.data;
+            giphs.forEach(giph => {
+                const imageURL = giph.images.downsized.url;
+                $('#results').append(`<img src="${imageURL}" alt="">`);
+            });
+        })
+        .fail(console.warn);
     });
 });
