1bc907f0080e180545eb686f1dc8ed9439ebbdc3
nimisaya <nimisaya@outlook.com>
2020-12-15 20:11:24 +1100

    Construct url for api call

--- amanda_jarvinen/week_07/giphy_api/js/main.js
+++ amanda_jarvinen/week_07/giphy_api/js/main.js
@@ -4,6 +4,17 @@ $(document).ready(function(){
 
     $('#searchBtn').on('click', function(){
         const searchTerm = $('#searchTerm').val();
-        console.log(searchTerm)
+        console.log(searchTerm);
+
+        const apiKey = '0KxC2vDq0SdT9gaua6lotHAFwYw7M5KR';
+        const baseURL = 'http://api.giphy.com/v1/gifs/search?q=';
+
+        const url = baseURL + searchTerm + '&api_key=' + apiKey
+
+        console.log(url)
+
+        // $.getJSON()
+        // .done()
+        // .fail(console.warn);
     });
 });
